3c84cfdd9ab1605adc9e746d7c7f9334caed0013
Mir <mir.m.ali01@gmail.com>
2023-09-09 23:04:36 -0400

    new route for fetching individual elements

--- server/routes/ElementRoutes.js
+++ server/routes/ElementRoutes.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const Element = require("../models/Element");
+const AllElements = require("../models/AllElements");
 
 // Define your API endpoint to get elements
 router.get("/elements", async (req, res) => {
   try {
     const elements = await Element.find();
     res.json(elements);
+  } catch (error) {
+    console.error("Error fetching elements:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.get("/allElements", async (req, res) => {
+  try {
+    const allElements = await AllElements.find();
+    res.json(allElements);
   } catch (error) {
     console.error("Error fetching elements:", error);
     res.status(500).json({ error: "Internal server error" });
--- server/server.js
+++ server/server.js
@@ -5,7 +5,9 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const ElementRouter = require("./routes/ElementRoutes");
 const dbConfig = require("./config/mongoDB");
+const AllElements = require("./models/AllElements");
 const Element = require("./models/Element");
+const fs = require("fs");
 
 const app = express();
 
@@ -20,60 +22,28 @@ mongoose
     console.log("Connected to MongoDB");
 
     // Check if data exists before seeding
-    const elementsCount = await Element.countDocuments();
-    if (elementsCount === 0) {
-      // Seed data example
-      const seedData = [
-        {
-          name: "Hydrogen",
-          appearance: "colorless gas",
-          atomic_mass: 1.008,
-          boil: 20.271,
-          category: "diatomic nonmetal",
-          density: 0.08988,
-          discovered_by: "Henry Cavendish",
-          melt: 13.99,
-          molar_heat: 28.836,
-          named_by: "Antoine Lavoisier",
-          number: 1,
-          period: 1,
-          group: 1,
-          phase: "Gas",
-          source: "https://en.wikipedia.org/wiki/Hydrogen",
-          bohr_model_image:
-            "https://storage.googleapis.com/search-ar-edu/periodic-table/element_001_hydrogen/element_001_hydrogen_srp_th.png",
-          bohr_model_3d:
-            "https://storage.googleapis.com/search-ar-edu/periodic-table/element_001_hydrogen/element_001_hydrogen.glb",
-          spectral_img:
-            "https://en.wikipedia.org/wiki/File:Hydrogen_Spectra.jpg",
-          summary:
-            "Hydrogen is a chemical element with chemical symbol H and atomic number 1. With an atomic weight of 1.00794 u, hydrogen is the lightest element on the periodic table. Its monatomic form (H) is the most abundant chemical substance in the Universe, constituting roughly 75% of all baryonic mass.",
-          symbol: "H",
-          xpos: 1,
-          ypos: 1,
-          wxpos: 1,
-          wypos: 1,
-          shells: [1],
-          electron_configuration: "1s1",
-          electron_configuration_semantic: "1s1",
-          electron_affinity: 72.769,
-          electronegativity_pauling: 2.2,
-          ionization_energies: [1312],
-          cpk_hex: "ffffff",
-          image: {
-            title:
-              "Vial of glowing ultrapure hydrogen, H2. Original size in cm: 1 x 5",
-            url: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Hydrogenglow.jpg",
-            attribution:
-              "User:Jurii, CC BY 3.0 <https://creativecommons.org/licenses/by/3.0>, via Wikimedia Commons, source: https://images-of-elements.com/hydrogen.php",
-          },
-          block: "s",
-        },
-      ];
+    // if (elementsCount === 0) {
+    // const elementsCount = await Element.countDocuments();
+    const allElementsCount = await AllElements.countDocuments();
+    if (allElementsCount === 0) {
+      try {
+        const rawData = fs.readFileSync("template.json");
+        const seedData = JSON.parse(rawData);
 
-      // Insert seed data into MongoDB
-      await Element.insertMany(seedData);
-      console.log("Seed data inserted");
+        // await Element.insertMany(seedData);
+        await AllElements.create(seedData);
+
+        // Extract elements from seedData and save them individually
+        const elementsData = seedData.data;
+
+        for (const elementData of elementsData) {
+          await Element.create(elementData);
+        }
+
+        console.log("Seed data inserted");
+      } catch (error) {
+        console.error("Error reading or inserting data:", error);
+      }
     } else {
       console.log("Database already contains data, skipping seed");
     }
